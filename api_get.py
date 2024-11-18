140d9b370cc95eda46263bc258ce93b5d98b7b85
Tony DiMambro <TonyGit5@mail.com>
2024-11-18 15:45:46 -0500

    Mail API -display source, title, description and url

--- api_get.py
+++ api_get.py
@@ -2,15 +2,42 @@
 import json
 import requests
 from datetime import datetime
+import send_email as mail
 
 start_date = "2024-10-18"
-API_KEY = "97597560372349699384541deea30fd8"
-url = "https://newsapi.org/v2/everything?" \
-    f"q=tesla&from={start_date}&sortBy=publishedAt&apiKey={API_KEY}"
+topic = "tesla"
+block = False
 
-#print (url)
+def api_call(start_date, topic):
+    
+    API_KEY = "97597560372349699384541deea30fd8"
+    url = "https://newsapi.org/v2/everything?" \
+        f"q={topic}&from={start_date}&language=en&sortBy=publishedAt" \
+            f"&apiKey={API_KEY}"
 
-request = requests.get(url)
-content = request.text
+    #print (url)
 
-print (content)
+    request = requests.get(url)
+    content = request.json()
+
+    return (content)
+
+def parser(content):
+    pass
+
+if __name__ == "__main__":
+    content = api_call(start_date, topic)
+    print (content['status'])
+    print (content['totalResults'])
+  
+    for article in content['articles']:
+        #print (f"{article['source']['name']} -- {article['title']} \n {article['description']} \n {article['url']} \n")
+        mail_content = (f"{article['source']['name']} -- {article['title']} \n {article['description']} \n {article['url']} \n")
+    
+    #mail_content = parser(content)
+    #mail.send_mail(mail_content)
+    
+    
+    #
+    #
+    #print (content['articles'])
