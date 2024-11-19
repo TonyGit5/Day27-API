fe2d788d4cc740514199d8958aaa944d90c597d0
Tony DiMambro <TonyGit5@mail.com>
2024-11-19 18:22:31 -0500

    add automatic start date and change to top_headlines

--- api_get.py
+++ api_get.py
@@ -6,18 +6,28 @@ import send_email as mail
 import unicodedata
 from unidecode import unidecode
 
-start_date = "2024-10-19"
-topic = "tesla"
+#start_date = "2024-10-19"
+country = "us"
+topic = ""
+sources = "slashdot"
+pageSize = 100
 block = False
 mail_content = []
 
+start_date = datetime.today().strftime('%Y-%m-%d')
 
 def api_call(start_date, topic):
     
     API_KEY = "97597560372349699384541deea30fd8"
-    url = "https://newsapi.org/v2/everything?" \
+    '''
+    url = "https://newsapi.org/v2/everyting?" \
         f"q={topic}&from={start_date}&language=en&sortBy=publishedAt" \
             f"&apiKey={API_KEY}"
+    '''
+    url = "https://newsapi.org/v2/top-headlines?" \
+        f"q={topic}&from={start_date}&language=en&country={country}&sortBy=publishedAt" \
+            f"&apiKey={API_KEY}"
+
 
     #print (url)
 
@@ -40,7 +50,8 @@ if __name__ == "__main__":
 
     for article in content['articles']:
         #print (f"{article['source']['name']} -- {article['title']} \n {article['description']} \n {article['url']} \n")
-        mail_content.append(str(f"{article['title']}  --  {article['source']['name']}\n {article['description']} \n {article['url']} \n"))
+        #mail_content.append(str(f"{article['title']}  --  {article['source']['name']}\n {article['description']} \n {article['url']} \n"))
+        mail_content.append(str(f"{article['title']}\n {article['description']} \n {article['url']} \n"))
     #print (mail_content)
     #mail_content = mail_content.encode("utf-8")
     mail_data = ""
