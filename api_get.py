d006282b76da760808ad90464622b2cc2d929966
Tony DiMambro <TonyGit5@mail.com>
2024-11-19 18:06:28 -0500

    e-mail Headline, Source, Descr, and article url

--- api_get.py
+++ api_get.py
@@ -3,10 +3,14 @@ import json
 import requests
 from datetime import datetime
 import send_email as mail
+import unicodedata
+from unidecode import unidecode
 
-start_date = "2024-10-18"
+start_date = "2024-10-19"
 topic = "tesla"
 block = False
+mail_content = []
+
 
 def api_call(start_date, topic):
     
@@ -26,16 +30,25 @@ def parser(content):
     pass
 
 if __name__ == "__main__":
+
     content = api_call(start_date, topic)
+    print (content)
     print (content['status'])
     print (content['totalResults'])
   
+    # Gather Article Source, Title, Description and url
+
     for article in content['articles']:
         #print (f"{article['source']['name']} -- {article['title']} \n {article['description']} \n {article['url']} \n")
-        mail_content = (f"{article['source']['name']} -- {article['title']} \n {article['description']} \n {article['url']} \n")
-    
+        mail_content.append(str(f"{article['title']}  --  {article['source']['name']}\n {article['description']} \n {article['url']} \n"))
+    #print (mail_content)
+    #mail_content = mail_content.encode("utf-8")
+    mail_data = ""
     #mail_content = parser(content)
-    #mail.send_mail(mail_content)
+    for item in mail_content:
+        mail_data = mail_data + unidecode(item)
+                    
+    mail.send_mail(mail_data)
     
     
     #
---
+++ send_email.py
@@ -0,0 +1,31 @@
+import smtplib, ssl
+import os
+
+block = False
+
+def send_mail(mail):
+    host = "smtp.gmail.com"
+    port = 465
+
+
+    receiver = "tdimambro@comcast.net"
+    message = f"""\
+        Subject: Daily News \n\n 
+        
+        {mail}
+    """
+
+    with open ("cred.txt") as cred:
+        creds = cred.readlines()
+        user = creds[0].strip("\n")
+        password = creds[1]
+        #password = os.getenv("PASSWD")
+    #print (creds)
+    #print (user, password)
+
+    if not block:
+        context = ssl.create_default_context()
+        #...context=context => name_of_variable = variable keyword=variable in the .SMTP_SSL Method
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(user, password)
+            server.sendmail(user, receiver, message)
