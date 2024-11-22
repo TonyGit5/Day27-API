2f5ee95bc6167aeeffa599c1728aeb1da800951a
Tony DiMambro <TonyGit5@mail.com>
2024-11-22 16:12:16 -0500

    Add Get_binary script

---
+++ get_binary.py
@@ -0,0 +1,17 @@
+import requests
+
+''' 
+This is how you get a file like a 
+jpg or other binary file.
+'''
+def get_binary(url):
+
+    binary = requests.get(url)
+    filename = str(url).split("/")[-1]
+
+    with open (filename, "wb") as file:
+        file.write(binary.content)
+
+if __name__ == "__main__":
+    url = "https://services.swpc.noaa.gov/images/animations/ovation/north/latest.jpg"
+    get_binary(url)
